50831fe66ac6b9d27ee1fe952555e05467244c0b
cungnv <cungnv@d-soft.com.vn>
2020-08-30 23:06:27 +0700

    add get booking by user id

--- src/main/java/vn/com/pn/screen/f002Booking/repository/BookingRepository.java
+++ src/main/java/vn/com/pn/screen/f002Booking/repository/BookingRepository.java
@@ -10,7 +10,7 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     @Query(value = "SELECT * FROM bookings WHERE user_id = ?1", nativeQuery = true)
     List<Booking> getListBookingHistory(Long userId);
 
-    @Query(value = "SELECT * FROM bookings WHERE DATE(check_in_date) = CURDATE() OR DATE(check_in_date) > CURDATE()" +
-            " OR DATE(check_out_date) = CURDATE() OR DATE(check_out_date) > CURDATE() AND user_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM bookings WHERE DATE(check_in_date) = CURRENT_DATE OR DATE(check_in_date) > CURRENT_DATE" +
+            " OR DATE(check_out_date) = CURRENT_DATE OR DATE(check_out_date) > CURRENT_DATE AND user_id = ?1", nativeQuery = true)
     List<Booking> getBookingByCurrentDateAndUser(Long userId);
 }
