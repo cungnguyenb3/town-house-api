77bf4ccdf4135e371fdfda60a771e7d5c879a4e5
cungnv <cungnv@d-soft.com.vn>
2020-08-31 23:02:55 +0700

    update query

--- src/main/java/vn/com/pn/screen/f002Booking/repository/BookingRepository.java
+++ src/main/java/vn/com/pn/screen/f002Booking/repository/BookingRepository.java
@@ -10,7 +10,10 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     @Query(value = "SELECT * FROM bookings WHERE user_id = ?1", nativeQuery = true)
     List<Booking> getListBookingHistory(Long userId);
 
-    @Query(value = "SELECT * FROM bookings WHERE DATE(check_in_date) = CURRENT_DATE OR DATE(check_in_date) > CURRENT_DATE" +
-            " OR DATE(check_out_date) = CURRENT_DATE OR DATE(check_out_date) > CURRENT_DATE AND user_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM bookings INNER JOIN hosts ON hosts.id = bookings.host_id" +
+                " INNER JOIN users ON hosts.agent_id = users.id" +
+            " WHERE DATE(bookings.check_in_date) = CURRENT_DATE OR DATE(bookings.check_in_date) > CURRENT_DATE" +
+            " OR DATE(bookings.check_out_date) = CURRENT_DATE OR DATE(bookings.check_out_date) > CURRENT_DATE" +
+            " AND hosts.agent_id = ?1", nativeQuery = true)
     List<Booking> getBookingByCurrentDateAndUser(Long userId);
 }
