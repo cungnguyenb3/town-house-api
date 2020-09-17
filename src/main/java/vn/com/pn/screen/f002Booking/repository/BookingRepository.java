7bc8433212f43ff15b6ec5485983416e166417e0
cungnv <cungnv@d-soft.com.vn>
2020-09-17 18:23:41 +0700

    s

--- src/main/java/vn/com/pn/screen/f002Booking/repository/BookingRepository.java
+++ src/main/java/vn/com/pn/screen/f002Booking/repository/BookingRepository.java
@@ -11,11 +11,9 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     @Query(value = "SELECT * FROM bookings WHERE user_id = ?1 order by created_at DESC", nativeQuery = true)
     List<Booking> getListBookingHistory(Long userId);
 
-    @Query(value = "SELECT * FROM bookings INNER JOIN hosts ON hosts.id = bookings.host_id" +
-                " INNER JOIN users ON hosts.agent_id = users.id" +
-            " WHERE DATE(bookings.check_in_date) = CURRENT_DATE OR DATE(bookings.check_in_date) > CURRENT_DATE" +
-            " OR DATE(bookings.check_out_date) = CURRENT_DATE OR DATE(bookings.check_out_date) > CURRENT_DATE" +
-            " AND hosts.agent_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM bookings INNER JOIN hosts ON hosts.id = bookings.host_id \n" +
+            "             WHERE hosts.agent_id = ?1 AND (DATE(bookings.check_in_date) = CURRENT_DATE OR DATE(bookings.check_in_date) > CURRENT_DATE \n" +
+            "             OR DATE(bookings.check_out_date) = CURRENT_DATE OR DATE(bookings.check_out_date) > CURRENT_DATE);", nativeQuery = true)
     List<Booking> getBookingByCurrentDateAndUser(Long userId);
 
     @Query(value = "SELECT * FROM bookings INNER JOIN hosts ON hosts.id = bookings.host_id" +
